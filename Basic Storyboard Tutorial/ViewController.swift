5255c3b5aabbef672137dddeb5bb924aace684f1
Waheduzzaman <waheduzzaman@Admins-MacBook-Pro-2.local>
2017-08-15 00:33:07 +0600

    first commit

--- Basic Storyboard Tutorial/ViewController.swift
+++ Basic Storyboard Tutorial/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //outlet is actually the reference for the view that we can access and modify
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    @IBAction func onTFClicked(_ sender: UITextField) {
+        outputLabel.text = "Please enter your name above";
+    }
+    
+    var textVal: String = "";
+    
+    //action is the function which is invoked on a uiComponent as defined
+    @IBAction func okClicked(_ sender: UIButton) {
+        textVal = textField.text!;
+        outputLabel.text = textVal;
+        textField.text = "";
+        self.view.endEditing(true);
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // do some network call 
+        // do some small checks
+        outputLabel.text = "Please enter your name above";
     }
 
     override func didReceiveMemoryWarning() {
